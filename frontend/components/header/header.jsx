72c28f00c533d515af1bfdd8cf454d41f6708b66
Edcarl Adraincem <iamedcarl@users.noreply.github.com>
2017-08-22 14:53:58 -0400

    Refactor header

--- frontend/components/header/header.jsx
+++ frontend/components/header/header.jsx
@@ -2,22 +2,30 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 class Header extends React.Component {
+  renderLoggedIn() {
+    return(
+      <div className='header-logged-in'>
+        <h1><Link to="/">feedRSS</Link></h1>
+        <button onClick={this.props.logout}>Logout</button>
+      </div>
+    );
+  }
+
+  renderLoggedOut() {
+    return(
+      <div className='header-logged-out'>
+        <h1><Link to="/">feedRSS</Link></h1>
+        <Link to="/login">Login</Link>
+        <Link to="/signup">Sign Up</Link>
+      </div>
+    );
+  }
+
   render() {
     if (this.props.loggedIn) {
-      return(
-        <div className='header-logged-in'>
-          <h1><Link to="/">feedRSS</Link></h1>
-          <button onClick={this.props.logout}>Logout</button>
-        </div>
-      );
+      return this.renderLoggedIn();
     } else {
-      return(
-        <div className='header-logged-out'>
-          <h1><Link to="/">feedRSS</Link></h1>
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-        </div>
-      );
+      return this.renderLoggedOut();
     }
   }
 
