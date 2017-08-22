3ed6e23d3e31de903284cde348c9db0559244464
Edcarl Adraincem <iamedcarl@users.noreply.github.com>
2017-08-22 09:47:28 -0400

    Complete static root page. User and session routes added

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  username        :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  img_url         :string
+#  fname           :string
+#  lname           :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
