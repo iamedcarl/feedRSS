c44832ff53664552aabe86d42222e4c6d70bfebc
Edcarl Adraincem <iamedcarl@users.noreply.github.com>
2017-08-23 12:03:32 -0400

    Complete features section on landing page

--- frontend/components/landing/landing.jsx
+++ frontend/components/landing/landing.jsx
@@ -4,13 +4,15 @@ import { Link } from 'react-router-dom';
 const LandingPage = () => {
   return(
     <div className='landing-page'>
+
       <header className='welcome-header'>
         <div className='welcome container'>
           <h1>
             Welcome to FeedRSS
           </h1>
           <div className='welcome-sub'>
-            The content you need to accelerate your research, marketing, and sales
+            The content you need to accelerate
+            your research, marketing, and sales
           </div>
           <div className='welcome-button'>
             <Link to="/signup">Get started for free</Link>
@@ -22,6 +24,42 @@ const LandingPage = () => {
           </div>
         </div>
       </header>
+
+      <div className='list-features'>
+        <div className='app-features container'>
+            <div className='app-features-kicker'>
+              You are in control
+            </div>
+            <h2>More news, less other stuff</h2>
+
+            <div className='app-features-grid'>
+              <div className='feature-1'>
+                <div className='feature-1-img'></div>
+                <h3>Your News Feed</h3>
+                <p>Whether it's real news, fake news, gossip, or
+                  more - don't miss out from the news feeds your trust.
+                </p>
+              </div>
+              <div className='feature-2'>
+                <div className='feature-2-img'></div>
+                <h3>Your Collections</h3>
+                <p>Keep things organized in specific categories so your
+                  fake news doesn't get mixed in with your real news...
+                  because that's important!
+                </p>
+              </div>
+              <div className='feature-3'>
+                <div className='feature-3-img'></div>
+                <h3>Your Control</h3>
+                <p>Read when you want and what you want. You can save articles to
+                  read while doing things or share with friends. 
+                </p>
+              </div>
+            </div>
+
+        </div>
+      </div>
+
     </div>
   );
 };
