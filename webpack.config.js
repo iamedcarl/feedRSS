06ec1f91205b7a677e4b820fb173e8e332698476
Edcarl Adraincem <iamedcarl@users.noreply.github.com>
2017-09-12 17:49:43 -0400

    Change let and const to var as it is not supported in heroku yet

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
-const path = require("path");
-const webpack = require("webpack");
+var path = require("path");
+var webpack = require("webpack");
 
-let plugins = [];
-let devPlugins = [];
+var plugins = [];
+var devPlugins = [];
 
-let prodPlugins = [
+var prodPlugins = [
   new webpack.DefinePlugin({
     'process.env': {
       'NODE_ENV': JSON.stringify('production')
